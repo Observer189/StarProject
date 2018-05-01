175062a2b72dd22b15db40b79c17e146635413e4
Observer189 <aleksandr.kozlovskiy03@mail.ru>
2018-05-01 14:43:25 +0300

    Something

--- core/src/com/mygdx/game/view/MainMenu.java
+++ core/src/com/mygdx/game/view/MainMenu.java
@@ -2,6 +2,7 @@ package com.mygdx.game.view;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
@@ -10,6 +11,11 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.mygdx.game.utils.TextManager;
 
 /**
@@ -17,11 +23,15 @@ import com.mygdx.game.utils.TextManager;
  */
 
 public class MainMenu implements Screen {
+    public float y=20/(float)(Gdx.graphics.getWidth()/Gdx.graphics.getHeight());
+    StageForButton sfb;
     SpriteBatch batch;
     Game game;
     TextureRegion img;
     private TextureAtlas textureAtlas;
    TextManager textManager;
+   public Button.ButtonStyle p_button;
+
 
 
 
@@ -43,8 +53,12 @@ public class MainMenu implements Screen {
        textManager = new TextManager(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
         img = new TextureRegion(textureAtlas.findRegion("Dakkar"));
 
-
-        
+        Skin skin=new Skin();
+        skin.addRegions(new TextureAtlas(Gdx.files.internal("TexturePack.atlas")));
+        p_button=new Button.ButtonStyle();
+        p_button.up= skin.getDrawable("Hunter");
+        p_button.down= skin.getDrawable("Hunter");
+        sfb=new StageForButton();
 
     }
 
@@ -54,9 +68,13 @@ public class MainMenu implements Screen {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         textManager.displayMessage(batch,"Hello, Dan",Color.BLUE,50,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+
+
         batch.begin();
         batch.draw(img,100,100);
 
+        sfb.act(delta);
+        sfb.draw();
 
         batch.end();
 
@@ -86,10 +104,20 @@ public class MainMenu implements Screen {
     @Override
     public void dispose() {
         batch.dispose();
+        sfb.dispose();
+        textureAtlas.dispose();
 
     }
 
     public void setTextureAtlas(TextureAtlas textureAtlas) {
         this.textureAtlas = textureAtlas;
+    }
+    class StageForButton extends Stage{
+        public StageForButton() {
+            super(new FitViewport(Gdx.graphics.getWidth(),  Gdx.graphics.getHeight()));
+            Button play=new Button(p_button);
+            play.setBounds(250,250,200,200);
+            addActor(play);
+        }
     }
 }
