85d9e419badfa87bf57db3e82365a661d00a10d4
Denis <denhol2002@mail.ru>
2018-05-25 17:14:35 +0300

    preferences

--- core/src/com/mygdx/game/utils/Assets.java
+++ core/src/com/mygdx/game/utils/Assets.java
@@ -1,5 +1,7 @@
 package com.mygdx.game.utils;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
@@ -9,12 +11,23 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public class Assets
 {
+
     private AssetManager manager;
+    public static Preferences preferences;
+    public static String Log;
+    public  static String Pass;
     public Assets()
     {
         manager=new AssetManager();
         manager.load("TexturePack.atlas",TextureAtlas.class);
         manager.finishLoading();
+    }
+    public static void  load(){
+        preferences= Gdx.app.getPreferences("DB1");
+      //  preferences.putString("Log","");
+      // preferences.putString("Pass","");
+        Log=preferences.getString("Log","");
+        Pass=preferences.getString("Pass","");
     }
 
     public AssetManager getManager() {
