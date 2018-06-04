5c9ba6d7cefb1aedf85b09611725049ac4525a83
Observer189 <aleksandr.kozlovskiy03@mail.ru>
2018-06-04 17:43:34 +0300

    Sudden

---
+++ core/src/com/mygdx/game/model/Ships/Sudden.java
@@ -0,0 +1,32 @@
+package com.mygdx.game.model.Ships;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.mygdx.game.model.FixingPoint;
+import com.mygdx.game.model.Ship;
+import com.mygdx.game.model.Weapons.BlueImpulseLaser;
+import com.mygdx.game.model.Weapons.Shotgun;
+
+/**
+ * Created by Sash on 04.06.2018.
+ */
+
+public class Sudden extends Ship {
+
+
+    @Override
+    public void draw(SpriteBatch batch) {
+        super.draw(batch);
+    }
+
+    public Sudden(TextureAtlas textureAtlas, float x, float y) {
+        super(textureAtlas.findRegion("Sudden"), x, y, 10, 22, "Sudden", 60000, 1400,
+                1.1f, 50, 1.3f, new FixingPoint[]{
+                        new FixingPoint(x, y, 10, 22, 2, 3, new Shotgun(textureAtlas, x, y)),
+                        new FixingPoint(x, y, 10, 22, 2, -3, new Shotgun(textureAtlas, x, y)),
+                        new FixingPoint(x, y, 10, 22, 6, 1, new Shotgun(textureAtlas, x, y)),
+                        new FixingPoint(x, y, 10, 22, 6, -1, new Shotgun(textureAtlas, x, y))
+                });
+
+    }
+}
